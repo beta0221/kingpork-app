5720d23eea15115d4425ef78cde28303929d9a99
beta0221 <beta0221@gmail.com>
2026-02-10 00:48:51 +0800

    強制日間模式

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class MyApp extends StatelessWidget {
 
       theme: AppTheme.lightTheme(context),
       darkTheme: AppTheme.darkTheme(context),
-      themeMode: ThemeMode.system,
+      themeMode: ThemeMode.light,
       onGenerateRoute: router.generateRoute,
       initialRoute: entryPointScreenRoute,
 
